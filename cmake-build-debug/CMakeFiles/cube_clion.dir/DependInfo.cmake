2a77f1d68fe83eefc2ee7dada957180c84d01732
Xenon1019 <jupadhyay02@gmail.com>
2021-03-07 23:14:29 +0530

    clion project files

---
+++ cmake-build-debug/CMakeFiles/cube_clion.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xenon/Desktop/stuff/programming/git/cube_clion/main.cpp" "/home/xenon/Desktop/stuff/programming/git/cube_clion/cmake-build-debug/CMakeFiles/cube_clion.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cube_clion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cube_clion.dir/main.cpp.o"
+  "cube_clion"
+  "cube_clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cube_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
